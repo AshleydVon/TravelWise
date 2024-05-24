67025e5051c47cae95986921352d43e0c77772f8
Monsieur <christopher.chhim123@gmail.com>
2024-05-23 17:24:47 -0700

    changed variable name and stored user input

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
-const countryEl = document.querySelector('#country');
-let country= prompt("Enter desired country: ");
-let countryWeather=fetch(`api.openweathermap.org/data/2.5/forecast?q=${country}&appid={25382a741e6bced20fc1f59a53126a82}`);
+const cityEl = document.querySelector('#weather');
+let city= document.getElementById('city');
+let cityWeather=fetch(`api.openweathermap.org/data/2.5/forecast?q=${city}&appid={25382a741e6bced20fc1f59a53126a82}`);
 
-function displayCountry(){
-    console.log(country);
-    return country;
+function displayCity(){
+    console.log(city);
+    return city;
 }
 
 function promptUser(){
-    if (country != ""){
-        window.prompt("Please enter a country");
-    } else if (country !=countryWeather) {
-        window.prompt("Weather for this country is not available, please select a different country.")
+    if (city != ""){
+        window.prompt("Please enter a city");
+    } else if (city !=cityWeather) {
+        window.prompt("Weather for this city is not available, please select a different city.")
     }
 }
 
+localStorage.setItem('city', document.getElementById('city').value);
